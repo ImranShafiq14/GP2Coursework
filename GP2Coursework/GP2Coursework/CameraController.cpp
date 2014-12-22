f5b72643cbd2e8a7df34d9fca718bed0abeefd6d
RossTaggart <rosstaggart12@gmail.com>
2014-12-22 01:22:51 +0000

    Quick documentation on Camera stuff
    More extensive documentation of ALL code will be in final commits
    towards the end of the projects life cycle

--- GP2Coursework/GP2Coursework/CameraController.cpp
+++ GP2Coursework/GP2Coursework/CameraController.cpp
@@ -31,16 +31,20 @@ void CameraController::update()
 {
 	if (attachedCamera)
 	{
+		//pulls values from camera - RT
 		vec3 currentPosition = attachedCamera->getParent()->getTransform()->getPosition();
 		vec3 currentRotation = attachedCamera->getParent()->getTransform()->getRotation();
 		vec3 currentLook = attachedCamera->getLookAt();
 
+		//pulls mouse values from mouse - rt
 		int mouseX = Input::getInput().getMouse()->getRelativeMouseX();
 		int mouseY = Input::getInput().getMouse()->getRelativeMouseY();
 
+		//calculates new rotation based on mouse values, time elapsed and speed of look - RT
 		currentRotation.x += mouseX * Timer::getTimer().getDeltaTime() * lookSpeed;
 		currentRotation.y += mouseY * Timer::getTimer().getDeltaTime() * lookSpeed;
 
+		//creates vector of where player is looking - RT
 		vec3 direction(cos(currentRotation.y) * sin(currentRotation.x),
 			sin(currentRotation.y),
 			cos(currentRotation.y) * cos(currentRotation.x));
@@ -56,6 +60,7 @@ void CameraController::update()
 			(float)attachedCamera->getParent()->getTransform()->getPosition().y << " " << 
 			(float)attachedCamera->getParent()->getTransform()->getPosition().z << endl;*/
 
+		//polls keyboard input from user and moves camera as appropriate
 		if (Input::getInput().getKeyboard()->isKeyDown(SDLK_w))
 		{
 			currentPosition.z += direction.z * forwardSpeed * Timer::getTimer().getDeltaTime();
@@ -76,10 +81,12 @@ void CameraController::update()
 			currentPosition -= right * strafeSpeed * Timer::getTimer().getDeltaTime();
 			//currentLook += right*(strafeSpeed*-1)*Timer::getTimer().getDeltaTime();
 		}
+		//toggles debug mode based on user input - RT
 		else if (Input::getInput().getKeyboard()->isKeyDown(SDLK_p))
 		{
 			debugMode = !debugMode;
 		}
+		//moves y axis if user in debug mode based on mouse scroll - RT
 		else if (Input::getInput().getMouse()->getMouseScroll() != 0)
 		{
 			if (debugMode)
@@ -95,6 +102,7 @@ void CameraController::update()
 			}
 		}
 
+		//updates camera values- RT
 		attachedCamera->getParent()->getTransform()->setPosition(currentPosition.x, currentPosition.y, currentPosition.z);
 		attachedCamera->getParent()->getTransform()->setRotation(currentRotation.x, currentRotation.y, currentRotation.z);
 		attachedCamera->setUp(up.x, up.y, up.z);
